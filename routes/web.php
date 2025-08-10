5a633892cbf1e9f6aa5203e31a0723ad4bf38f3c
tiesen243 <ttien56906@gmail.com>
2025-08-10 21:55:59 +0700

    feat: CRUD post (2)

--- routes/web.php
+++ routes/web.php
@@ -9,5 +9,7 @@ return [
   ['GET', '/posts/new', [PostController::class, 'create']],
   ['POST', '/posts/create', [PostController::class, 'store']],
   ['GET', '/posts/{id}', [PostController::class, 'show']],
+  ['GET', '/posts/{id}/edit', [PostController::class, 'edit']],
+  ['POST', '/posts/{id}/update', [PostController::class, 'update']],
   ['POST', '/posts/{id}/delete', [PostController::class, 'delete']],
 ];
