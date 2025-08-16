6ae59046f8e2aa47b09e5ad01a09325cab37077c
tiesen243 <ttien56906@gmail.com>
2025-08-17 04:33:00 +0700

    chore: remove seed in build

--- seed.php
+++ seed.php
@@ -35,6 +35,7 @@ class Seeder
     if (!file_exists($envFile)) {
       return;
     }
+
     $lines = file($envFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
     foreach ($lines as $line) {
       if (str_starts_with($line, '#')) {
